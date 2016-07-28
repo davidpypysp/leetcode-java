5fda1acab5dffc6a89e70288dcd66823ad25adff
davidpysp <davidpy1992@gmail.com>
2016-07-27 18:56:57 -0700

    206

--- src/main/java/com/shiping/leetcode/hard/DungeonGame.java
+++ src/main/java/com/shiping/leetcode/hard/DungeonGame.java
@@ -8,7 +8,7 @@ public class DungeonGame {
         int n = dungeon.length, m = dungeon[0].length;
         int dp[][] = new int[n][m];// dp[i][j]: minimum hp to enter (i, j)
 
-        dp[n-1][m-1] = Math.max(-dungeon[n-1][m-1], 0) + 1;// right-bottom node
+        dp[n-1][m-1] = Math.max(-dungeon[n-1][m-1], 0) + 1;// r-bottom node
         for (int i = n - 2; i >= 0; i--) // last row
             dp[i][m-1] = Math.max(1, dp[i+1][m-1] - dungeon[i][m-1]);
         for (int j = m - 2; j >= 0; j--) // last column
@@ -16,7 +16,7 @@ public class DungeonGame {
 
         for (int i = n - 2; i >= 0; i--) {// calculate dp table
             for (int j = m - 2; j >= 0; j--) {
-                dp[i][j] = Math.min(dp[i][j+1], dp[i+1][j]);// right, down
+                dp[i][j] = Math.min(dp[i][j+1], dp[i+1][j]);// r, down
                 dp[i][j] = Math.max(1, dp[i][j] - dungeon[i][j]);
             }
         }
--- src/main/java/com/shiping/leetcode/hard/RecoverBinarySearchTree.java
+++ src/main/java/com/shiping/leetcode/hard/RecoverBinarySearchTree.java
@@ -1,7 +1,37 @@
 package com.shiping.leetcode.hard;
 
+import com.shiping.leetcode.util.TreeNode;
+
 /**
  * Created by shipingci on 7/27/16.
  */
 public class RecoverBinarySearchTree {
+    TreeNode pre = null;
+    TreeNode l = null, r = null;
+
+    public void recoverTree(TreeNode root) {
+        inOrderTraversal(root);
+        int tmp = l.val;
+        l.val = r.val;
+        r.val = tmp;
+    }
+
+
+    public void inOrderTraversal(TreeNode root) {
+        if(root == null) return;
+        inOrderTraversal(root.left);
+
+        if(pre != null && root.val < pre.val) {
+            if(l == null) {
+                l = pre;
+                r = root;
+            } else {
+                r = root;
+            }
+        }
+        pre = root;
+        inOrderTraversal(root.right);
+    }
+
+
 }
